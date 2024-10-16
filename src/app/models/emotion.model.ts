1649b86dd736a92498fd3fb1d9529e8f44bddad9
joycelaura <j.ramrezmuoz2@uandresbello.edu>
2024-10-15 22:22:01 -0300

    creacion de estructura de botones emocionales

---
+++ src/app/models/emotion.model.ts
@@ -0,0 +1,5 @@
+export interface Emotion {
+    name: string;        // Nombre de la emoción
+    count: number;       // Contador de cuantas veces se seleccionó
+    lastUpdated: Date;   // Última vez que fue actualizada
+  }
