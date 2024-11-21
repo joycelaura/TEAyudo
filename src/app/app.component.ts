23825f7f002d37592c3d9380351326c7f2ddf58d
Migueliedo <162817805+Migueliedo1610@users.noreply.github.com>
2024-11-21 00:53:43 -0300

    creacion del esqueleto del grafico
    se creo el esqueleto del grafico en la page de emociones, en un futuro debe visualizar un grafico de las emociones del mes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';"@angular/core";
 import { ThemeService } from './services/theme.service';
+import { AudioService } from "./services/audio.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
   styleUrls: ["app.component.scss"],
 })
-export class AppComponent {
-  constructor(private themeService: ThemeService) {
+export class AppComponent implements OnInit {	
+  constructor(private themeService: ThemeService, private audioService: AudioService) {
     // Cargar el tema al iniciar la aplicación
     this.themeService.loadTheme();
+  }
+
+  async ngOnInit() {
+    await this.audioService.loadSound('alegria', 'assets/sounds/alegria.mp3');
+    await this.audioService.loadSound('tristeza', 'assets/sounds/tristeza.mp3');
+    await this.audioService.loadSound('ira', 'assets/sounds/ira.mp3');
+    await this.audioService.loadSound('agrado', 'assets/sounds/agrado.mp3');
+    await this.audioService.loadSound('contencion', 'assets/sounds/contencion.mp3');
+    await this.audioService.loadSound('soledad', 'assets/sounds/soledad.mp3');
+    await this.audioService.loadSound('miedo', 'assets/sounds/miedo.mp3');
+    await this.audioService.loadSound('verguenza', 'assets/sounds/verguenza.mp3');
   }
 }
--- src/app/pages/inicio/calendario/calendario.page.ts
+++ src/app/pages/inicio/calendario/calendario.page.ts
@@ -1,30 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { Chart, ChartData, ChartOptions,  } from 'chart.js';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { Emotion } from 'src/app/models/emotion.model';
+import { ThemeService } from "src/app/services/theme.service";
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.page.html',
   styleUrls: ['./calendario.page.scss'],
 })
-export class CalendarioPage implements OnInit {
+export class CalendarioPage implements OnInit, AfterViewInit {
+
+  @ViewChild('emotionChart') emotionChart: any; // Referencia al canvas
+  chart: any; // Referencia a la instancia del gráfico
+
+  // Datos y opciones de la gráfica
+  chartData: ChartData<'bar'> = {
+    labels: [], // Etiquetas de las emociones
+    datasets: [{
+      data: [], // Datos de las emociones
+      label: 'Emociones',
+      backgroundColor: '#42A5F5', // Color de las barras
+      borderColor: '#1E88E5', // Color del borde de las barras
+      borderWidth: 1,
+    }],
+  };
+
+  chartOptions: ChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false, // Asegura que no se mantenga la relación de aspecto
+    scales: {
+      x: {
+        beginAtZero: true,
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+    // Opcional: establecer un tamaño específico
+    plugins: {
+      legend: {
+        position: 'top',
+      }
+    }
+  };
+  
+
+
   days: number[] = [];
   currentMonth: number = new Date().getMonth();  // Inicializar con el mes actual
   currentYear: number = new Date().getFullYear();  // Inicializar con el año actual
   monthEmotions: { [key: string]: Emotion[] } = {};
 
+  weekDays: string[] = ['L', 'M', 'X', 'J', 'V', 'S', 'D'];  // Días de la semana
+  calendarDays: any[] = [];  // Arreglo para organizar los días en semanas
   monthNames: string[] = [
     'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
     'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
   ];
-
   yearRange: number[] = [];
 
-  constructor(private firestoreSvc: FirestoreService) {}
+  constructor(private firestoreSvc: FirestoreService, private themeService: ThemeService) {}
 
   ngOnInit() {
     this.generateYearRange();  // Generar el rango de años
     this.generateCalendar();    // Generar el calendario con el mes y año actuales
+    this.generateEmotionStats(); // Generar las estadísticas emocionales
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.initChart(); // Inicializa el gráfico después de un breve retraso
+    }, 0);
+  }
+
+  async applyStoredTheme() {
+    const currentTheme = this.themeService.getCurrentTheme();
+    document.body.classList.add(currentTheme);
+  }
+
+  // Inicializar el gráfico
+  initChart() {
+    const chartCanvas = this.emotionChart.nativeElement;
+    if (chartCanvas) {
+      this.chart = new Chart(chartCanvas, {
+        type: 'bar',
+        data: this.chartData,
+        options: this.chartOptions,
+      });
+    }
+  }
+  
+
+  // Actualizar la gráfica cuando cambian los datos
+  updateChart() {
+    if (this.chart) {
+      this.chart.data = this.chartData;
+      this.chart.update();
+    }
   }
 
   // Generar un rango de años para el selector de años
@@ -39,39 +112,37 @@ export class CalendarioPage implements OnInit {
   }
 
   // Generar el calendario basado en el mes y año seleccionados
+  // Generar el calendario basado en el mes y año seleccionados
   async generateCalendar() {
-    const daysInMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDate();
-    this.days = Array.from({ length: daysInMonth }, (_, i) => i + 1);
-
+    const daysInMonth = new Date(this.currentYear, this.currentMonth + 1, 0).getDate(); // Total de días en el mes
+    const firstDayOfMonth = new Date(this.currentYear, this.currentMonth, 1).getDay(); // Día de la semana del primer día del mes
+  
+    // Ajustar el primer día para que la semana comience en lunes
+    const firstDayAdjusted = firstDayOfMonth === 0 ? 6 : firstDayOfMonth - 1; // Si es domingo (0), ajustamos a 6 (sábado)
+  
+    // Crear un array de días con días vacíos al inicio según el primer día de la semana
+    const daysArray = [];
+    for (let i = 0; i < firstDayAdjusted; i++) {
+      daysArray.push(null); // Rellenamos con null para los días vacíos antes del 1 del mes
+    }
+    for (let day = 1; day <= daysInMonth; day++) {
+      daysArray.push(day); // Agregar los días del mes
+    }
+  
+    // Dividir los días en semanas (array de arrays)
+    this.calendarDays = [];
+    while (daysArray.length) {
+      this.calendarDays.push(daysArray.splice(0, 7)); // Divide en semanas (7 días por semana)
+    }
+  
     const email = localStorage.getItem('userEmail');
     if (email) {
       this.monthEmotions = await this.firestoreSvc.getEmotionsByMonth(email, this.currentYear, this.currentMonth);
     }
   }
-  // Actualizar el calendario cuando el usuario cambia mes o año
-  updateCalendar() {
-    this.generateCalendar();
-  }
+  
+  
 
-  // Navegar al siguiente mes
-  nextMonth() {
-    this.currentMonth = (this.currentMonth + 1) % 12;
-    if (this.currentMonth === 0) {
-      this.currentYear += 1;
-    }
-    this.generateCalendar();
-  }
-
-  // Navegar al mes anterior
-  prevMonth() {
-    this.currentMonth = (this.currentMonth - 1 + 12) % 12;
-    if (this.currentMonth === 11) {
-      this.currentYear -= 1;
-    }
-    this.generateCalendar();
-  }
-
-  // Obtener el ícono de la emoción dominante para un día específico
   getDominantEmotionIcon(day: number): string | null {
     const dateKey = `${this.currentYear}-${(this.currentMonth + 1).toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
     const emotions = this.monthEmotions[dateKey];
@@ -82,4 +153,48 @@ export class CalendarioPage implements OnInit {
     }
     return null;
   }
+
+  async generateEmotionStats() {
+    const emotionCounts: { [key: string]: number } = {};
+
+    for (const dateKey in this.monthEmotions) {
+      const emotions = this.monthEmotions[dateKey];
+      if (emotions) {
+        emotions.forEach(emotion => {
+          emotionCounts[emotion.name] = (emotionCounts[emotion.name] || 0) + 1;
+        });
+      }
+    }
+
+    const emotions = Object.keys(emotionCounts);
+    const counts = emotions.map(emotion => emotionCounts[emotion]);
+
+    this.chartData.labels = emotions;
+    this.chartData.datasets[0].data = counts;
+
+    this.updateChart(); // Actualizar el gráfico con los nuevos datos
+  }
+  
+// Actualizar el calendario cuando el usuario cambia mes o año
+updateCalendar() {
+  this.generateCalendar();
+}
+
+// Navegar al siguiente mes
+nextMonth() {
+  this.currentMonth = (this.currentMonth + 1) % 12;
+  if (this.currentMonth === 0) {
+    this.currentYear += 1;
+  }
+  this.generateCalendar();
+}
+
+// Navegar al mes anterior
+prevMonth() {
+  this.currentMonth = (this.currentMonth - 1 + 12) % 12;
+  if (this.currentMonth === 11) {
+    this.currentYear -= 1;
+  }
+  this.generateCalendar();
+}
 }
